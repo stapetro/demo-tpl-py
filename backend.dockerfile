fda70245182b8f5a05d4680f60f6a70d716e2867
Stanislav Petrov <stanislav.petrov@gfk.com>
2021-03-05 16:19:54 +0200

    [dbg] Check build in docker

--- backend.dockerfile
+++ backend.dockerfile
@@ -16,5 +16,4 @@ ARG INSTALL_DEV=false
 RUN bash -c "if [ $INSTALL_DEV == 'true' ] ; then poetry install --no-root ; else poetry install --no-root --no-dev ; fi"
 
 COPY ./src/app /app/app
-RUN ls -al /app
 ENV PYTHONPATH=/app
--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -x
 
 docker container stop demo-tpl-py-svc; docker container rm demo-tpl-py-svc
-docker run -d --name demo-tpl-py-svc -p 8889:80 demo-tpl-py || exit $?
+docker run -d --rm --name demo-tpl-py-svc -p 8889:80 demo-tpl-py || exit $?
